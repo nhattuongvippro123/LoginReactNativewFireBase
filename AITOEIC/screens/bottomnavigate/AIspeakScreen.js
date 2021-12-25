0002574164efe7301ef717c40b64d298f79f1aea
nhattuongvippro123 <38066063+nhattuongvippro123@users.noreply.github.com>
2021-12-25 22:28:20 +0700

    update AISPEAK
    25/12/2021

--- AITOEIC/screens/bottomnavigate/AIspeakScreen.js
+++ AITOEIC/screens/bottomnavigate/AIspeakScreen.js
@@ -9,6 +9,7 @@ import {
   TouchableOpacity,
   ImageBackground,
   Dimensions,
+  Image,
 } from 'react-native';
 import Slider from '@react-native-community/slider';
 import Header from './../../components/Header';
@@ -18,10 +19,13 @@ import {Platform} from 'react-native';
 import RNFS from 'react-native-fs';
 import FormButton from './../../components/FormButton';
 import {TextInput, Button} from 'react-native-paper';
+import {launchImageLibrary} from 'react-native-image-picker';
+import TextRecognition from 'react-native-text-recognition';
 
 export default function AIspeakScreen() {
   const [text, setText] = useState();
   const [newpath, setPath] = useState();
+  const [image, setImage] = useState(null);
   const path = `${RNFS.DocumentDirectoryPath}/voice8.mp3`;
   // const path = `${RNFS.DocumentDirectoryPath}/voice5.mp3`;
   const Sound = require('react-native-sound');
@@ -62,7 +66,6 @@ export default function AIspeakScreen() {
         if (newpath) {
           playMusic(newpath);
           deleteFile(path);
-
         }
       })
       .catch((error) => {
@@ -109,6 +112,16 @@ export default function AIspeakScreen() {
       });
     };
   };
+  const Scan = async () => {
+    // const result = await TextRecognition.recognize(image?.assets[0]?.uri);
+    const result1 = 'Enclave';
+    setText(result1);
+  };
+  console.log(image);
+  useEffect(() => {
+    if (image) {
+    }
+  }, [image]);
 
   return (
     <ImageBackground
@@ -117,37 +130,101 @@ export default function AIspeakScreen() {
       <Header title="AI SPEAK" />
       <View
         style={{
+          padding: 5,
           flexDirection: 'column',
           height: '100%',
         }}>
         <View
-          style={{flex: 1, alignCenter: 'center', justifyContent: 'center'}}>
+          style={{
+            flex: 1,
+            alignCenter: 'center',
+            justifyContent: 'center',
+            padding: 5,
+          }}>
           <Text
             style={{
-              padding: 25,
-              fontSize: 28,
+              fontSize: 17,
               color: '#000044',
             }}>
-            Bạn có thể viết một câu hoặc dán một đoạn văn mà bạn thích AISPEAK
-            đọc cho bạn vào ô trống bên dưới.
+            Bạn có thể viết một câu hoặc dán một đoạn văn mà bạn thích vào ô
+            trống bên dưới.
           </Text>
-        </View>
-        <View style={{flex: 1, padding: 25}}>
-          <TextInput
-            style={styles.textInput}
-            onChangeText={(text) => setText(text)}
-            value={text}
-            placeholder="Nhập ở đây ..."
-            onSubmitEditing={Keyboard.dismiss}
-          />
-          <Button
-            style={{marginTop: 20}}
-            color="#8ad24e"
-            icon="volume-high"
-            mode="contained"
-            onPress={() => texttospeech()}>
-            Bấm cái là đọc như thiệt luôn nè!
-          </Button>
+          <Text
+            style={{
+              fontSize: 17,
+              color: '#000044',
+            }}>
+            Hoặc bạn có thể chọn ảnh từ thư viện, AI SPEAK sẽ đọc cho bạn nghe.
+          </Text>
+          <Text style={{marginLeft: 25, marginTop: 10, fontSize: 15}}>
+            Chọn ảnh bạn muốn scan:
+          </Text>
+          <View
+            style={{
+              marginTop: 10,
+              flexDirection: 'row',
+              alignCenter: 'center',
+              justifyContent: 'center',
+              marginLeft: 7,
+              marginBottom: 20,
+            }}>
+            <TouchableOpacity
+              style={{
+                flex: 1,
+                width: windowWidth / 2,
+              }}
+              mode="contained"
+              onPress={() => launchImageLibrary({}, setImage)}>
+              <Image
+                source={require('../../assets/imageAISPEAK/folder.png')}
+                resizeMode="contain"
+                style={{
+                  width: 150,
+                  height: 100,
+                  borderRadius: 10,
+                }}
+              />
+            </TouchableOpacity>
+            <Image
+              source={{uri: image?.assets[0]?.uri}}
+              resizeMode="contain"
+              style={{
+                marginLeft: 50,
+                flex: 1,
+                width: 250,
+                height: 100,
+                borderRadius: 10,
+              }}
+            />
+            <Button
+              style={{
+                margin: 30,
+                alignCenter: 'center',
+                justifyContent: 'center',
+              }}
+              color="#8ad24e"
+              mode="contained"
+              onPress={() => Scan()}>
+              Scan
+            </Button>
+          </View>
+          <View style={{flex: 1, padding: 25}}>
+            <TextInput
+              style={styles.textInput}
+              onChangeText={(text) => setText(text)}
+              value={text}
+              placeholder="Nhập ở đây ..."
+              onSubmitEditing={Keyboard.dismiss}
+            />
+            <Button
+              style={{marginTop: 20}}
+              color="#8ad24e"
+              icon="volume-high"
+              mode="contained"
+              onPress={() => texttospeech()}>
+              Bấm cái là đọc như thiệt luôn nè!
+            </Button>
+          </View>
         </View>
       </View>
     </ImageBackground>
